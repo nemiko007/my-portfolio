e661783520f5b02c17532f271b044e2f3f1771e8
nemiko007 <otoha.youjopero2@gmail.com>
2026-01-20 12:55:16 +0900

    デプロイ設定

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,14 @@
 import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
+import vercel from '@astrojs/vercel/server'; // Import the Vercel adapter
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server', // Set output to server
+  adapter: vercel(), // Use the Vercel adapter
   integrations: [react()],
 
   vite: {
